9f67cce442e15cb5b0093d14aa14c24990bb4a89
Danny Feller <59851021+486DX2-boomer@users.noreply.github.com>
2023-03-03 14:48:36 -0500

    Reorder links

--- src/QuickLinks.tsx
+++ src/QuickLinks.tsx
@@ -1,17 +1,8 @@
 const quickLinks = [
   {
-    name: "Shooter Born in Heaven Ranges",
-    url: "https://imgur.com/a/LAWSkJk?fbclid=IwAR09r_hgLEgVMqnllMUOJcw5ebSjr-jo-2FYLIFi7sTGhpzH0ojmFjtySis",
-  },
-  {
     name: "Extracts",
     url: "https://eftfg.com/extractions",
   },
-  {
-    name: "Items to Keep",
-    url: "https://eftfg.com/items",
-  },
-  { name: "Quests Tree", url: "https://eft.monster/quest-tree" },
   {
     name: "Ammo Chart",
     url: "https://eft-ammo.com/",
@@ -21,12 +12,21 @@ const quickLinks = [
     url: "https://tarkov.dev/",
   },
   {
+    name: "Items to Keep",
+    url: "https://eftfg.com/items",
+  },
+  { name: "Quests Tree", url: "https://eft.monster/quest-tree" },
+  {
     name: "Tarkov Tracker",
     url: "https://tarkovtracker.io/",
   },
   {
     name: "Ballistics Calculator",
     url: "https://calculator.tarkov-ballistics.com/",
+  },
+  {
+    name: "Shooter Born in Heaven Ranges",
+    url: "https://imgur.com/a/LAWSkJk?fbclid=IwAR09r_hgLEgVMqnllMUOJcw5ebSjr-jo-2FYLIFi7sTGhpzH0ojmFjtySis",
   },
 ];
 
